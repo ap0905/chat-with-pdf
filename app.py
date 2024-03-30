3130c880235a41c3c346dec10b4763de17a196ca
ap0905 <149944573+ap0905@users.noreply.github.com>
2024-03-30 18:16:11 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+ 
 import streamlit as st
+from dotenv import load_dotenv
+import pickle
 from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -7,41 +11,45 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
-import pickle
-
+ 
 # Sidebar contents
-st.sidebar.title('LLM Chat App')
-st.sidebar.markdown('''
-## About
-This app is an LLM-powered chatbot built using:
-- [Streamlit](https://streamlit.io/)
-- [LangChain](https://python.langchain.com/)
-- [OpenAI](https://platform.openai.com/docs/models) LLM model
-
-Made by Alfin
-''')
-
+with st.sidebar:
+    st.title('LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+ 
+    ''')
+    add_vertical_space(5)
+    st.write('Made by Alfin')
+ 
+load_dotenv()
+ 
 def main():
     st.header("Chat with PDF 💬")
-
-    # Upload a PDF file
+ 
     pdf = st.file_uploader("Upload your PDF", type='pdf')
-
+ 
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
+        
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
-
+ 
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200,
             length_function=len
-        )
+            )
         chunks = text_splitter.split_text(text=text)
-
+ 
         store_name = pdf.name[:-4]
-
+        st.write(f'{store_name}')
+ 
         if os.path.exists(f"{store_name}.pkl"):
             with open(f"{store_name}.pkl", "rb") as f:
                 VectorStore = pickle.load(f)
@@ -50,16 +58,18 @@ def main():
             VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
             with open(f"{store_name}.pkl", "wb") as f:
                 pickle.dump(VectorStore, f)
-
+ 
         query = st.text_input("Ask questions about your PDF file:")
-
+        
         if query:
             docs = VectorStore.similarity_search(query=query, k=3)
-            llm = OpenAI()  # Initialize your LLM model here
+ 
+            llm = OpenAI()
             chain = load_qa_chain(llm=llm, chain_type="stuff")
             with get_openai_callback() as cb:
                 response = chain.run(input_documents=docs, question=query)
+                print(cb)
             st.write(response)
-
+ 
 if __name__ == '__main__':
     main()
